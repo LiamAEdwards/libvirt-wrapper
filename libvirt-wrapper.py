68a3cd7bf9b8dc2e27302f86220ee1a4dc4ec80a
LiamAEdwards <liam.a.edwards@protonmail.com>
2023-01-24 21:08:31 +0000

    Running a VM from userinput now working

--- libvirt-wrapper.py
+++ libvirt-wrapper.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader
+import libvirt
 
 def main():
 
@@ -7,14 +8,14 @@ def main():
   template = environment.get_template("default.xml")
 
   # variables to read from user
-  name        = input("Enter the VMs name. - ")
-  description = input("Enter a description. - ")
-  memory      = input("How much memory? In MB E.g. 4096 - ")
-  vcpus       = input("How many virtual CPUs E.g. 2 - ")
-  path        = input("What is the absolute path for the QCOW image? - ")
-  iso         = input("What is the absolute path for the Linux ISO - ")
+  name        = input("Enter the VMs name. - ") or 'Test'
+  description = input("Enter a description. - ") or 'Testing 123'
+  memory      = input("How much memory? In MB E.g. 4096 - ") or 4096
+  vcpus       = input("How many virtual CPUs E.g. 2 - ") or 4
+  path        = input("What is the absolute path for the QCOW image? - ") or '/var/lib/libvirt/images/centos8'
+  iso         = input("What is the absolute path for the Linux ISO - ") or '/var/lib/libvirt/images/CentOS-Stream-9-latest-x86_64-dvd1.iso'
   
-  filename = f"message_{test.lower()}.xml"
+  filename = f"virt-xml.xml"
   content = template.render(
       name=name,
       description=description,
@@ -26,5 +27,14 @@ def main():
   with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
     print(f"... wrote {filename}")
+
+  with open(filename) as fp:
+    xmlconfig = fp.read()
+
+  conn = libvirt.open('qemu:///system')
+  instance = conn.defineXML(xmlconfig)
+
+  instance.create()
+  instance.isActive()
 
 main()
