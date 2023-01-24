752e4abc68e29bf58fb1c317626f4333fada313b
LiamAEdwards <liam.a.edwards@protonmail.com>
2023-01-24 21:18:33 +0000

    Trying to clean up user inputs

--- libvirt-wrapper.py
+++ libvirt-wrapper.py
@@ -8,12 +8,12 @@ def main():
   template = environment.get_template("default.xml")
 
   # variables to read from user
-  name        = input("Enter the VMs name. - ") or 'Test'
-  description = input("Enter a description. - ") or 'Testing 123'
-  memory      = input("How much memory? In MB E.g. 4096 - ") or 4096
-  vcpus       = input("How many virtual CPUs E.g. 2 - ") or 4
-  path        = input("What is the absolute path for the QCOW image? - ") or '/var/lib/libvirt/images/centos8'
-  iso         = input("What is the absolute path for the Linux ISO - ") or '/var/lib/libvirt/images/CentOS-Stream-9-latest-x86_64-dvd1.iso'
+  name        = input("Enter the VMs name.                           (Default will be 'Test')\n") or 'Test'
+  description = input("Enter a description.                          (Default will be 'Testing 123')\n") or 'Testing 123'
+  memory      = input("How much memory? In MiB E.g. 4096             (Default will be 4096)\n") or 4096
+  vcpus       = input("How many virtual CPUs E.g. 2                  (Default will be 4)\n") or 4
+  path        = input("What is the absolute path for the QCOW image? (Default will be '/var/lib/libvirt/images/centos8'\n") or '/var/lib/libvirt/images/centos8'
+  iso         = input("What is the absolute path for the Linux ISO   (Default will be '/var/lib/libvirt/images/CentOS-Stream-9-latest-x86_64-dvd1.iso'\n") or '/var/lib/libvirt/images/CentOS-Stream-9-latest-x86_64-dvd1.iso'
   
   filename = f"virt-xml.xml"
   content = template.render(
@@ -26,7 +26,7 @@ def main():
   )
   with open(filename, mode="w", encoding="utf-8") as message:
     message.write(content)
-    print(f"... wrote {filename}")
+    print(f"Created file {filename}")
 
   with open(filename) as fp:
     xmlconfig = fp.read()
